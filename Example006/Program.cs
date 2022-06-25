0c57f8157401b55c0c137158b2a51b80f9ea90c9
IgorGuselnikov <guselnikov.uniikm@yandex.ru>
2022-06-25 22:13:17 +0500

    Find max digit

---
+++ Example006/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 2;
+int b = 5;
+int c = 16;
+int d = 12;
+int e = 4;
+int max = a;
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+Console.Write("max = ");
+Console.WriteLine(max);
