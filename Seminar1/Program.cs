93871f4bbfc01ddaf1fb18c6e55b507d6d8f58c6
Igor <guselnikov.uniikm@yandex.ru>
2022-06-30 00:17:52 +0500

    third task

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -7,7 +7,7 @@
 
 // Вторая задача
 
-Console.WriteLine("Введите первое целое число: ");
+/*Console.WriteLine("Введите первое целое число: ");
 int squareNumber = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе целое число: ");
 int number = Convert.ToInt32(Console.ReadLine());
@@ -19,4 +19,46 @@ if(squareNumber == number * number)
 else 
 {
     Console.WriteLine("Первое число не является квадратом второго");   
+}
+*/
+
+// Третья задача
+
+Console.WriteLine("введите день недели");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+if (dayNumber >= 1 && dayNumber <=7)
+{
+    if (dayNumber == 1)
+    {
+        Console.WriteLine("Понедельник");
+    }
+    if (dayNumber == 2)
+    {
+        Console.WriteLine("Вторник");
+    }
+    if (dayNumber == 3)
+    {
+        Console.WriteLine("Среда");
+    }
+    if (dayNumber == 4)
+    {
+        Console.WriteLine("Четверг");
+    }
+    if (dayNumber == 5)
+    {
+        Console.WriteLine("Пятница");
+    }
+    if (dayNumber == 6)
+    {
+        Console.WriteLine("Суббота");
+    }
+    if (dayNumber == 7)
+    {
+        Console.WriteLine("Воскресенье");
+    }
+}
+else
+{
+    Console.WriteLine("Введено неверное число недели");
 }
