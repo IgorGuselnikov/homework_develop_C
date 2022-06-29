405078b42698574a1ba26c267dddfcbb98f24501
Igor <guselnikov.uniikm@yandex.ru>
2022-06-30 00:05:00 +0500

    two task

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,4 +1,22 @@
-﻿int value = Convert.ToInt32(Console.ReadLine());
-int result = value * value;
+﻿//int value = Convert.ToInt32(Console.ReadLine());
+//int result = value * value;
 
-Console.WriteLine("Квадрат данного числа равен:" + result);
+//Console.WriteLine("Квадрат данного числа равен:" + result);
+
+
+
+// Вторая задача
+
+Console.WriteLine("Введите первое целое число: ");
+int squareNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(squareNumber == number * number)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else 
+{
+    Console.WriteLine("Первое число не является квадратом второго");   
+}
